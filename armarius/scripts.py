f35e251f06f32468767021e7b4622b83129db56a
Jae-Myoung Yu <euphoris@gmail.com>
2013-04-26 20:49:36 +0900

    run_server script

---
+++ armarius/scripts.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import os.path
+
+from . import app, initdb
+
+def run_server():
+    try:
+        config = os.path.abspath(sys.argv[1])
+        app.config.from_pyfile(config)
+    except:
+        cwd = os.getcwd()
+        app.config['DATABASE'] = 'sqlite:////' + os.path.join(cwd, 'wiki.db')
+
+    with app.app_context():
+        initdb()
+
+    port = app.config.get('PORT', 5000)
+
+    app.run(port=port)
